92edfa9acd0db7ae8e2546102bfa086a488a2c7a
ankit-08 <77257785+ankit-08@users.noreply.github.com>
2023-06-07 09:33:53 +0530

    certificates added

--- script.js
+++ script.js
@@ -71,3 +71,17 @@ $(document).ready(function(){
         }
     });
 });
+
+// script for image-gallery
+
+
+var fullImgBox = document.getElementById("fullImgBox");
+var fullImg = document.getElementById("fullImg");
+
+function openFullImg(pic) {
+    fullImgBox.style.display = "flex";
+    fullImg.src = pic;
+}
+function closeFullImg() {
+    fullImgBox.style.display = "none";
+}
